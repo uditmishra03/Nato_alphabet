b87ac99116e142b0af922a320e0720d0a4d08edc
Udit <uditmishra.um@gmail.com>
2022-03-05 17:25:01 +0530

    Added exception handling for TypeError

--- main.py
+++ main.py
@@ -1,44 +1,23 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-#
-# # Looping through dictionaries:
-# for (key, value) in student_dict.items():
-#     # Access key and value
-#     pass
-#
-
-#
-# student_data_frame = pandas.DataFrame(student_dict)
-#
-# # Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-#     # Access index and row
-#     # Access row.student or row.score
-#     pass
-#
-# # Keyword Method with iterrows()
-# # {new_key:new_value for (index, row) in df.iterrows()}
 import pandas
 
 
-def remove_spaces(word):
+def remove_spaces(text):
     text = text.strip()
     text_without_space = text.replace(" ", "")
     return text_without_space
 
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    word = remove_spaces(word)
+    try:
+        output_code_list = [nato_alphabet_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please. ")
+        generate_phonetic()
+    else:
+        print(output_code_list)
+
 
 nato_alphabet_data = pandas.read_csv("nato_phonetic_alphabet.csv")
-
-# TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet_data.iterrows()}
-# print(nato_alphabet_dict)
-
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-word = input("Enter a word: ").upper()
-word = remove_spaces(word)
-output_code_list = [nato_alphabet_dict[letter] for letter in word]
-print(output_code_list)
+generate_phonetic()
